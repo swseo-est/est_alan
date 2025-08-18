d6f3c3ab5d6ddcd55620ff179c2e3b6c809f3ca5
swseo <swseo.astro@gmail.com>
2025-08-18 15:23:31 +0900

    add finish status

--- src/estalan/agent/graph/slide_generate_agent/utils.py
+++ src/estalan/agent/graph/slide_generate_agent/utils.py
@@ -185,8 +185,8 @@ def clear_template_cache():
 # 기본 템플릿 디렉토리
 # 현재 스크립트의 디렉토리를 기준으로 절대 경로 생성
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-DEFAULT_TEMPLATE_DIR = os.path.join(SCRIPT_DIR, "slide_template", "template1")
-# DEFAULT_TEMPLATE_DIR = os.path.join(SCRIPT_DIR, "slide_template", "morden template")
+DEFAULT_TEMPLATE_DIR = os.path.join(SCRIPT_DIR, "slide_template", "general")
+DEFAULT_TEMPLATE_DIR = os.path.join(SCRIPT_DIR, "slide_template", "compare")
 
 
 def get_html_template_list() -> str:

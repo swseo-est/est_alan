64da62a7fb57c68d76421586303e5bd785311704
swseo <swseo.astro@gmail.com>
2025-08-22 16:11:41 +0900

    임시용 dummy 메시지 추가

--- src/estalan/agent/base/node.py
+++ src/estalan/agent/base/node.py
@@ -1,15 +1,18 @@
 from typing import Dict, Any
+from estalan.messages.utils import create_ai_message
 
 
 def alan_agent_start_node(state: Dict[str, Any]) -> Dict[str, Any]:
     metadata = dict()
     metadata["chat_status"] = "unavailable"
+    dummy = create_ai_message(content="")
 
-    return {"metadata": metadata}
+    return {"metadata": metadata, "messages": [dummy]}
 
 
 def alan_agent_finish_node(state: Dict[str, Any]) -> Dict[str, Any]:
     metadata = dict()
     metadata["chat_status"] = "available"
+    dummy = create_ai_message(content="")
 
-    return {"metadata": metadata}
+    return {"metadata": metadata, "messages": [dummy]}

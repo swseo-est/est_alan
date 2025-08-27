5a649d719e1fbbcc0698db7946aabcfb76cff671
swseo <swseo.astro@gmail.com>
2025-08-27 11:13:22 +0900

    api server 그래프 생성 방식 변경

--- src/deployment/slide_generate_agent/src/agent/graph.py
+++ src/deployment/slide_generate_agent/src/agent/graph.py
@@ -1,18 +1,6 @@
-import asyncio
 from estalan.agent.graph.slide_generate_agent.graph import create_graph
 
-_graph = None
-_lock = None
 
-
-async def get_graph():
-    global _graph, _lock
-
-    if _lock is None:
-        _lock = asyncio.Lock()  # 현재 이벤트 루프에서 Lock 생성
-
-    async with _lock:
-        if _graph is None:
-            _graph = create_graph()
-
+def get_graph():
+    _graph = create_graph()
     return _graph
